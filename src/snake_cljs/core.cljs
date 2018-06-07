a9d6d15bc65ab101b015defeb055a45bb29026ef
adyang <adyang2011@gmail.com>
2018-06-08 01:50:21 +0800

    Cleanup unused functions and configuration.

--- src/snake_cljs/core.cljs
+++ src/snake_cljs/core.cljs
@@ -3,16 +3,4 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/snake-cljs/core.cljs. Go ahead and edit it and see reloading in action. Really?")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
-
 (set! (.-onload js/window) start)
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  (swap! app-state update-in [:__figwheel_counter] inc))
-
